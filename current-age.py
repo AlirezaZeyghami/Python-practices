da117b292304a387f8b7d78175a20b4b20f11f20
Alireza <alireza.zeighami@gmail.com>
2025-10-05 23:03:36 +0330

    Current age

--- current-age.py
+++ current-age.py
@@ -3,14 +3,20 @@ from datetime import datetime
 birthdate = input().strip()
 
 try:
-    year, month, day = map(int, birthdate.split("/"))
-    birth_date = datetime(year, month, day)
+    birth_date = datetime.strptime(birthdate, "%Y/%m/%d")
 except ValueError:
     print("WRONG")
     exit()
 
 today = datetime.now()
 
+if birth_date > today:
+    print("WRONG")
+    exit()
+
 user_age = today.year - birth_date.year
+
+if (today.month, today.day) < (birth_date.month, birth_date.day):
+    user_age -= 1
 
 print(user_age)
