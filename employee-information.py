351647bf442b823321e0cd65046025e8864c8903
Alireza <alireza.zeighami@gmail.com>
2025-10-10 19:33:08 +0330

    Working with Databases in Python - (employee-information)

---
+++ employee-information.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+p1_db = mysql.connector.connect(user='********', password='********', host='localhost', database='employee_information')
+cursor = p1_db.cursor()
+
+queri = "SELECT name, height, weight FROM employees"
+cursor.execute(queri)
+
+employees = []
+for (name, height, weight) in cursor:
+    employees.append((name, height, weight))
+
+employees.sort(key=lambda x: (-x[1], x[2]))
+
+for name, height, weight in employees:
+    print(f"{name} {int(height)} {int(weight)}")
+
+p1_db.close()
